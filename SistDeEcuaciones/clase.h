a07b297235a20663f66c8f904c0989c02cf4ceb1
Juan <jiffavaron@gmail.com>
2024-09-03 19:37:01 -0300

    formated

--- SistDeEcuaciones/clase.h
+++ SistDeEcuaciones/clase.h
@@ -36,7 +36,7 @@ public:
     void print() const {
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < n+1; ++j) {
-                cout << data[i][j] << " ";
+                cout << data[i][j] << " \t";
             }
             cout << endl;
         }
